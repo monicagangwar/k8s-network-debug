3c5e14dbafecf2303fd401e56bfa771c0b8efee9
Monica Gangwar <monicagangwar10@gmail.com>
2019-08-08 12:04:07 +0530

    updating time elapsed calculation in the entrypoint

--- pod-startup/entrypoint.sh
+++ pod-startup/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-start_time=`date +%s`
+SECONDS=0
+duration=''
 service='kubernetes.default'
 output=0
 for i in {1..10}
@@ -7,21 +8,23 @@ do
     ping -W 1 -c 1 $service
     output=`echo $?`
     if [[ $output == 1 ]]; then
+        duration=$SECONDS
         echo "iteration $i: ping $service -> success."
         break
     else
-       echo "iteration $i: ping $service -> failed."
+        duration=$SECONDS
+        echo "iteration $i: ping $service -> failed."
     fi
     sleep 1
 done
-end_time=`date +%s`
-time_taken=$( expr $end_time - $start_time )
 
-echo $end_time $start_time
+time_taken="$(($duration / 60)) minutes and $(($duration % 60)) seconds"
+
 if [[ $output == 1 ]]; then
-    echo "Network setup in $time_taken seconds."
+    echo "Network setup in $time_taken."
     exit 0
 else
-   echo "All attempts exhausted. Pod network not setup in $time_taken seconds."
+   echo "All attempts exhausted. Pod network not setup in $time_taken."
    exit 1
 fi
+
